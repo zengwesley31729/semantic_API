5cea3e7e3549388783d4c55371accffda4a118b6
Huan-Chieh Tseng <52860119+zengwesley31729@users.noreply.github.com>
2022-05-06 19:48:44 +0800

    Update search_paper.py

--- search_paper.py
+++ search_paper.py
@@ -2,7 +2,8 @@ import sematic_API
 import sys
 import json
 
-
+#They are all parameters
+#you can choose whcih parameters you want
 fields_cate='paperId,externalIds,url,title,abstract,venue,year,referenceCount,citationCount,influentialCitationCount,isOpenAccess,fieldsOfStudy,s2FieldsOfStudy,authors'
 data = sematic_API.paper_search(offset=0,
                                 limit=None,
